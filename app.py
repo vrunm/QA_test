0fefcab2b1635ee745e41f66f32329ee3aba77e6
Varun Mathur <97465624+vrunm@users.noreply.github.com>
2023-02-27 11:36:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import streamlit as st
 df1 = pd.read_csv("https://raw.githubusercontent.com/vrunm/nlp-datasets/main/earnings_calls_data_sentences.csv")
-st.dataframe(data=df1, width=None, height=None, *, use_container_width=False)
+st.dataframe(data=df1)
